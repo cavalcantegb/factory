e6a69f7ecc1b7042acae52cbfbac88d248583372
Guilherme Bastos Cavalcante <cavalcante.guilherme91@gmail.com>
2020-05-16 15:12:17 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Fabrica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CardFactory factory = null;
+
+            Console.Write("Enter the card type you would like to visit: ");
+            string car = Console.ReadLine();
+
+            switch(car.ToLower())
+            {
+                case "moneyback":
+                    factory = new MoneyBackFactory(50000, 0);
+                    break;
+                case "titanium":
+                    factory = new TitaniumFactory(100000, 500);
+                    break;
+                default:
+                    break;
+            }
+
+            CreditCard creditCard = factory.GetCreditCard();
+            Console.WriteLine("\nYour card details are below : \n");
+            Console.WriteLine("Card Type: {0}\nCredit Limit: {1}\nAnnual Charge: {2}",
+                creditCard.CardType, creditCard.CreditLimit, creditCard.AnnualCharge);
+            Console.ReadKey();
+        }
+    }
+}
